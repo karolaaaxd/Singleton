b9b1b9e75349dc0c6931cc22ba7ef3206b7a3c4a
karolaaaxd <karolciaxd2011@onet.pl>
2021-03-28 17:53:52 +0200

    first commit

---
+++ source/repos/Singleton/Singleton/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Singleton obiekt1 = Singleton.GetObject();
+            Singleton obiekt2 = Singleton.GetObject();
+
+            
+            if (obiekt1 == obiekt2)
+            {
+                Console.WriteLine("są identyczne");
+            }
+            else
+            {
+                Console.WriteLine("są różne");
+            }
+
+        }
+    }
+
+    public sealed class Singleton
+    {
+        private static Singleton obiekt;
+
+     
+        private Singleton() { }
+
+       
+        public static Singleton GetObject()
+        {
+            if (obiekt == null)
+            {
+
+                obiekt = new Singleton();
+            }
+            
+            return obiekt;
+        }
+    }
+}
