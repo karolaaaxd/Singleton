6ca4727031dc3cca2f8a3e0002cdff44c9856565
karolaaaxd <karolciaxd2011@onet.pl>
2021-03-28 21:18:45 +0200

    factory method

---
+++ source/repos/FactoryMethod/FactoryMethod/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        abstract class Ciasto
+        {
+            public abstract string Upiecz();
+
+
+        }
+        class Beza : Ciasto
+        {
+            public override string Upiecz()
+            {
+
+                return "Upieczona beza";
+
+            }
+        }
+
+
+        class Murzynek : Ciasto
+        {
+            public override string Upiecz()
+            {
+                return "Upieczony murzynek";
+            }
+        }
+
+        class FabrykaCiast
+        {
+            public Ciasto produkcjaCiastek(string about)
+            {
+                Ciasto ciastko = null;
+
+                if (about.Equals("Beza"))
+                {
+                    ciastko = new Beza();
+                }
+                else if (about.Equals("Murzynek"))
+                {
+                    ciastko = new Murzynek();
+                }
+
+                return ciastko;
+            }
+
+        }
+        static void Main(string[] args)
+        {
+
+            FabrykaCiast producent = new FabrykaCiast();
+
+            Console.WriteLine(producent.produkcjaCiastek("Beza").Upiecz());
+            
+            Console.WriteLine(producent.produkcjaCiastek("Murzynek").Upiecz());
+
+
+        }
+    }
+}
